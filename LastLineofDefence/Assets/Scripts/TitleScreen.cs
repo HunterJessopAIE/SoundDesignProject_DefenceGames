6d8031cd325a5ebbfaa78a4f4c2b7781622922b1
BraydenZeman <brayden.zeman@gmail.com>
2021-07-18 15:24:32 +0930

    Title screen UI tweaked

---
+++ LastLineofDefence/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    public GameObject startMenu;
+    public GameObject howToMenu;
+    public GameObject customizeMenu;
+    public GameObject quitMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void StartButton()
+    {
+
+    }
+    void HowToButton()
+    {
+
+    }
+    void CustomizeButton()
+    {
+
+    }
+    void QuitButton()
+    {
+
+    }
+    void Close()
+    {
+        //if (startMenu.SetActive(true))
+        {
+            startMenu.SetActive(false);
+        }
+        
+    }
+}
