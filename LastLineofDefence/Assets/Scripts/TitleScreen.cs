2248914f3c5fdb11b0c042028b428bab385a069a
BraydenZeman <brayden.zeman@gmail.com>
2021-07-18 17:25:19 +0930

    Title Screen UI fixes

--- LastLineofDefence/Assets/Scripts/TitleScreen.cs
+++ LastLineofDefence/Assets/Scripts/TitleScreen.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
@@ -8,39 +9,18 @@ public class TitleScreen : MonoBehaviour
     public GameObject howToMenu;
     public GameObject customizeMenu;
     public GameObject quitMenu;
+    private int _currentScene;
+    
     // Start is called before the first frame update
-    void Start()
+   
+    public void StartButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void StartButton()
-    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
-    void HowToButton()
+    
+    public void QuitGame()
     {
-
-    }
-    void CustomizeButton()
-    {
-
-    }
-    void QuitButton()
-    {
-
-    }
-    void Close()
-    {
-        //if (startMenu.SetActive(true))
-        {
-            startMenu.SetActive(false);
-        }
-        
+        Application.Quit();
     }
 }
