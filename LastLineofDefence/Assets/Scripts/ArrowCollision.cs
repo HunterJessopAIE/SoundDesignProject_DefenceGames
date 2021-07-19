2935ebbe95ae4dc65f6c36ed53c817b993df4c2f
AIE\s210066 <s210066@students.aie.edu.au>
2021-07-19 12:07:00 +0930

    ShieldObject and script complete

--- LastLineofDefence/Assets/Scripts/ArrowCollision.cs
+++ LastLineofDefence/Assets/Scripts/ArrowCollision.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class ArrowCollision : MonoBehaviour
 {
-    public Rigidbody arrow;
-    public GameObject player;
-    public Rigidbody shield;
+    //==== COMPONENTS ====
+    public GameObject arrow;
+   
     private Vector3 _arrowSpeed;
     private Vector3 _arrowDirection;
     private Vector3 _arrowRotation;
 
-    void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Triggered");
-        if (other.tag == "Shield")
-        {
-            ReflectArrow();
-        }
-        Debug.Log("Failed");
-    }
+    //==== COLLISION PROTOCOL ====
     void OnCollisionEnter(Collision other)
     {
         /*
@@ -27,9 +19,7 @@ public class ArrowCollision : MonoBehaviour
          */
         if (other.gameObject.tag == "Ground")
         {
-            arrow.constraints = RigidbodyConstraints.FreezeAll;
-            arrow.velocity = Vector3.zero;
-            arrow.angularVelocity = Vector3.zero;
+            
             return;
         }
         /*
@@ -37,58 +27,46 @@ public class ArrowCollision : MonoBehaviour
          */
         if (other.gameObject.tag == "Warrior")
         {
-            arrow.constraints = RigidbodyConstraints.FreezeAll;
-            arrow.velocity = Vector3.zero;
-            arrow.angularVelocity = Vector3.zero;
+            
             DamageEnemy();
         }
 
         /*
          * HITS THE SHIELD
          */
-        Debug.Log("====SHIELD CHECK====");
-
         if (other.gameObject.tag == "Shield")
         {
             ReflectArrow();
+            Destroy(arrow);
         }
-        Debug.Log("====SHEILD CHECK 2====");
-        if (other.rigidbody == shield)
-        {
-            ReflectArrow();
-        }
+      
 
         /*
          * HITS THE PLAYER
          */
-        else if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
         {
             DamagePlayer();
         }
  
     }
    
-
+    //==== FUNCTIONS ====
     private void DamagePlayer()
     {
         Debug.Log("OW!");
-      //  GetComponent<PlayerController.FindObjectOfType>
+      
     }
     private void ReflectArrow()
     {
         Debug.Log("BING");
-        _arrowSpeed = arrow.velocity;
-        _arrowDirection = arrow.transform.position;
-        //_arrowRotation = arrow.transform.r
-        arrow.velocity = Vector3.zero;
-        arrow.angularVelocity = Vector3.zero;
-        //arrow.transform.Rotate
+        
        
     }
 
     private void DamageEnemy()
     {
         Debug.Log("Enemy Hit");
-        //Destroy(other.gameObject);
+        
     }
 }
---
+++ LastLineofDefence/Assets/Scripts/ShieldController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject shieldPosTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = shieldPosTracker.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = shieldPosTracker.transform.position;
+
+
+        this.transform.rotation = new Quaternion(player.transform.rotation.x, player.transform.rotation.y, player.transform.rotation.z, player.transform.rotation.w);
+
+    }
+}
