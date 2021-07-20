f212dd48476f5361388f43b8c3dc12237f375ce1
AIE\s210491 <s210491@students.aie.edu.au>
2021-07-20 10:43:53 +0930

    Game Manager script and text elements implemented

---
+++ LastLineofDefence/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public TMP_Text scoreText;
+
+    public GameObject endGameScreen;
+    public TMP_Text outcomeText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endGameScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
